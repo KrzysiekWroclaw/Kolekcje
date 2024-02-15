87cc4e65ef85c4557f066bdd74fccb2726874c8d
Krzysztof Czekaj <krzysztofczekaj@gmail.com>
2024-02-15 22:49:43 +0100

    Add project files.

---
+++ KalkulatorStatystyczny/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KalkulatorStatystyczny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
