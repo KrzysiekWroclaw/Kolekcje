1650f6139be6963898b3294bce39b5efae582286
Krzysztof Czekaj <krzysztofczekaj@gmail.com>
2024-03-10 02:54:27 +0100

    Wersja 1.

---
+++ ZbieranieUnikalnychSlow/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        HashSet<string> uniqueWords = new HashSet<string>();
+        Console.WriteLine("Program: Zbieranie unikalnych słów. Wprowadź tekst(wpisz \"koniec\", aby zakończyć):");
+        string? entireText = Console.ReadLine();
+
+        string[] words;
+        if (entireText != "koniec" && entireText.Length != 0)
+        {
+            words = entireText.Split(' ');
+        }
+        else
+        {
+            Console.WriteLine("Program zakończony.");
+            return;
+        }
+
+        Console.WriteLine("\nUnikalne słowa w tekście: ");
+        int i = 1;
+        foreach (string word in words)
+        {
+            while (!uniqueWords.Contains(word))
+            {
+                uniqueWords.Add(word);
+                Console.WriteLine($"{i}. {word}");
+                i++;
+            }
+
+        }
+    }
+}
