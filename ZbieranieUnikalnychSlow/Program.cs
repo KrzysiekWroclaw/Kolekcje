79b2b094e24151b1cfe33c3756f271a59cdf76a1
Krzysztof Czekaj <krzysztofczekaj@gmail.com>
2024-03-11 18:33:49 +0100

    użycie metody 'ToHashSet()' zamiast pętli 'foreach'

--- ZbieranieUnikalnychSlow/Program.cs
+++ ZbieranieUnikalnychSlow/Program.cs
@@ -3,8 +3,7 @@
 class Program
 {
     public static void Main(string[] args)
-    {
-        HashSet<string> uniqueWords = new HashSet<string>();
+    {       
         Console.WriteLine("Program: Zbieranie unikalnych słów. Wprowadź tekst(wpisz \"koniec\", aby zakończyć):");
         string? entireText = Console.ReadLine();
 
@@ -20,16 +19,12 @@ class Program
         }
 
         Console.WriteLine("\nUnikalne słowa w tekście: ");
+        HashSet<string> uniqueWords = new HashSet<string>();
         int i = 1;
-        foreach (string word in words)
+        uniqueWords = words.ToHashSet();
+        foreach (string word in uniqueWords)
         {
-            while (!uniqueWords.Contains(word))
-            {
-                uniqueWords.Add(word);
-                Console.WriteLine($"{i}. {word}");
-                i++;
-            }
-
+            Console.WriteLine($"{i++}. {word}");            
         }
     }
 }
