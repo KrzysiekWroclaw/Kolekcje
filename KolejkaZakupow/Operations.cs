c3bf5a9cb8501b25d57a00a85c6b01a408a3b5d8
Krzysztof Czekaj <krzysztofczekaj@gmail.com>
2024-03-13 11:48:02 +0100

    Utworzenie projektu "KolejkaZakupow".

---
+++ KolejkaZakupow/Operations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolejkaZakupow
+{
+    class Operations
+    {
+
+        public static List<string> GetNewBasket()
+        {
+            Console.WriteLine("Dodaj produkty do koszyka (oddzielone spacją): "); 
+
+            string? entireText = Console.ReadLine();
+            string[] shoppingTab = entireText.Split(' ');
+            List<string> basket = new List<string>();
+
+            foreach (string product in shoppingTab)
+            {
+                basket.Add(product);
+            }
+           
+            Console.WriteLine("Produkty dodane do koszyka!:\n");
+            return basket;
+           
+        }
+
+        public static Queue<List<string>> MoveBasketToKassQueue(Queue<List<string>> queueOfBaskets, Queue<List<string>> KassQueue)
+        {
+            while (queueOfBaskets.Count > 0)
+            {
+                List<string> products = queueOfBaskets.Dequeue();
+                KassQueue.Enqueue(products);
+                Console.WriteLine("Zawartość koszyka: "); 
+                foreach (var item in products)
+                {
+                    Console.Write($"{item} ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Obsłużony klient. Koszyk przesunięty do kolejki kasowej.\n");
+            }
+            return KassQueue;
+
+
+        }
+    }
+
+}
---
+++ KolejkaZakupow/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace KolejkaZakupow
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(
+@"Program: Kolejka zakupów.
+
+1. Nowy klient - dodaj produkty do koszyka
+2. Zakończ zakupy i obsłuż klienta
+3. Zakończ program");
+            Console.WriteLine();
+
+            List<string> basket = new List<string>();
+            Queue<List<string>> queueOfBaskets = new Queue<List<string>>();
+            Queue<List<string>> KassQueue = new Queue<List<string>>();
+
+
+            while (true)
+            {
+                Console.WriteLine("Wprowadź wartość od 1 do 3: ");
+                int operationNumber = Convert.ToInt32(Console.ReadLine());
+
+
+                if (operationNumber == 1)
+                {
+                    Console.WriteLine("Twój wybór: 1");
+                    basket = Operations.GetNewBasket();
+                    queueOfBaskets.Enqueue(basket);                    
+
+                }
+                if (operationNumber == 2)
+                {
+                    Console.WriteLine("Twój wybór: 2");
+                    Operations.MoveBasketToKassQueue(queueOfBaskets, KassQueue);
+                   
+                   
+                }
+                if (operationNumber == 3)
+                {
+
+                }
+                //else
+                //{
+                //    Console.WriteLine("Wprowadzono niepoprawną wartość. Wprowadź poprawną wartość: \n");
+                //}
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        //List<string> currentList = queue.Dequeue();
+        //foreach (string product in currentList)
+        //{
+        //    Console.WriteLine(product);
+        //}
+
+
+
+    }
+
+
+}
