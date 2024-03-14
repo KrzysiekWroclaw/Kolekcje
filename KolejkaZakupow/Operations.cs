8059a36a8e36f1648dea8a025f52b9dbb30dd44b
Krzysztof Czekaj <krzysztofczekaj@gmail.com>
2024-03-14 09:44:00 +0100

    Wersja 1 - cały kod działa.

--- KolejkaZakupow/Operations.cs
+++ KolejkaZakupow/Operations.cs
@@ -9,43 +9,97 @@ namespace KolejkaZakupow
 {
     class Operations
     {
-
         public static List<string> GetNewBasket()
         {
-            Console.WriteLine("Dodaj produkty do koszyka (oddzielone spacją): "); 
-
+            Console.WriteLine("Dodaj produkty do koszyka (oddzielone spacją): ");
             string? entireText = Console.ReadLine();
-            string[] shoppingTab = entireText.Split(' ');
-            List<string> basket = new List<string>();
+            string[] basketTab = entireText.Split(' ');
+            List<string> basketList = new List<string>(basketTab);
+            Queue<List<string>> basketsQueue = new Queue<List<string>>();
+            Queue<List<string>> kassQueue = new Queue<List<string>>();
 
-            foreach (string product in shoppingTab)
+            basketsQueue.Enqueue(basketList);
+
+            Console.WriteLine("Następujące produkty dodane zostały do kolejki klientów: ");
+
+            foreach (string product in basketList)
             {
-                basket.Add(product);
+                Console.Write($"{product} ");
             }
-           
-            Console.WriteLine("Produkty dodane do koszyka!:\n");
-            return basket;
-           
+            Console.WriteLine("\n");
+            return basketList;
         }
 
-        public static Queue<List<string>> MoveBasketToKassQueue(Queue<List<string>> queueOfBaskets, Queue<List<string>> KassQueue)
+        public static void MoveBasketToKassQueue(Queue<List<string>> basketsQueue, Queue<List<string>> kassQueue)
         {
-            while (queueOfBaskets.Count > 0)
+            List<string> basket;
+            if (basketsQueue.Count > 0)
             {
-                List<string> products = queueOfBaskets.Dequeue();
-                KassQueue.Enqueue(products);
-                Console.WriteLine("Zawartość koszyka: "); 
-                foreach (var item in products)
+                basket = basketsQueue.Dequeue();
+                kassQueue.Enqueue(basket);
+                Console.WriteLine("Obsłużony klient. Zawartość koszyka przeniesionego do kolejki kasowej: ");
+                foreach (string product in basket)
                 {
-                    Console.Write($"{item} ");
+                    Console.Write($"{product} ");
                 }
                 Console.WriteLine();
-                Console.WriteLine("Obsłużony klient. Koszyk przesunięty do kolejki kasowej.\n");
+                Console.WriteLine();
             }
-            return KassQueue;
-
-
+            else
+            {
+                Console.WriteLine("Kolejka koszyków jest pusta. Brak koszyków do przesunięcia.\n");
+            }
+        }
+        public static void DecisionYorN(Queue<List<string>> basketsQueue,
+                                        Queue<List<string>> kassQueue)
+        {
+            if (basketsQueue.Count > 0)
+            {
+                Console.WriteLine("Uwaga: nie wszystkie koszyki" +
+                    " zostały przesunięte do kolejki kasowej! " +
+                    " Czy chcesz je wszystkie teraz tam przesunąć? Wpisz \"tak/nie\"");
+                string? yesOrNo = Console.ReadLine();
+                if (yesOrNo == "tak")
+                {
+                    while (basketsQueue.Count > 0)
+                    {
+                        Operations.MoveBasketToKassQueue(basketsQueue, kassQueue);
+                    }
+                }
+                else if (yesOrNo == "nie")
+                {
+                    Operations.MoveBasketToKassQueue(basketsQueue, kassQueue);
+                }
+            }
+            else
+            {
+                Operations.MoveBasketToKassQueue(basketsQueue, kassQueue);
+            }        
+        }
+        public static void DisplayBasketsInKassQueue(Queue<List<string>> kassQueue)
+        {
+            if (kassQueue.Count > 0)
+            {
+                Console.WriteLine("Lista koszyków w kolejce kasowej: ");
+                int i = 1;
+                foreach (List<string> list in kassQueue)
+                {
+                    Console.Write($"{i}. ");
+                    int j = 1;
+                    foreach (var item in list)
+                    {
+                        Console.Write($"{item}");
+                        if (j < (list.Count))
+                        {
+                            Console.Write(", ");
+                            j++;
+                        }
+                    }
+                    i++;
+                    Console.WriteLine();
+                }
+            }            
+            Console.WriteLine("Program zakończony.");
         }
     }
-
 }
--- KolejkaZakupow/Program.cs
+++ KolejkaZakupow/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
 
 namespace KolejkaZakupow
-{
-    class Program
+{    class Program
     {
         public static void Main(string[] args)
         {
@@ -16,60 +16,47 @@ namespace KolejkaZakupow
 3. Zakończ program");
             Console.WriteLine();
 
-            List<string> basket = new List<string>();
-            Queue<List<string>> queueOfBaskets = new Queue<List<string>>();
-            Queue<List<string>> KassQueue = new Queue<List<string>>();
-
+            List<string> basketList = new List<string>();
+            Queue<List<string>> basketsQueue = new Queue<List<string>>();
+            Queue<List<string>> kassQueue = new Queue<List<string>>();
 
             while (true)
             {
-                Console.WriteLine("Wprowadź wartość od 1 do 3: ");
-                int operationNumber = Convert.ToInt32(Console.ReadLine());
-
-
+                int operationNumber;
+                while (true)
+                {
+                    Console.WriteLine($"Wprowadź wartość od 1 do 3: ");
+                    string? stringOperationNumber = Console.ReadLine();
+                    if (int.TryParse(stringOperationNumber, out operationNumber))
+                    {break;}
+                    else {Console.WriteLine("Wprowadzono niepoprawną wartość.\n");}
+                    break;
+                }
                 if (operationNumber == 1)
                 {
                     Console.WriteLine("Twój wybór: 1");
-                    basket = Operations.GetNewBasket();
-                    queueOfBaskets.Enqueue(basket);                    
+                    basketList = Operations.GetNewBasket();
+                    basketsQueue.Enqueue(basketList);
 
                 }
-                if (operationNumber == 2)
+                else if (operationNumber == 2)
                 {
                     Console.WriteLine("Twój wybór: 2");
-                    Operations.MoveBasketToKassQueue(queueOfBaskets, KassQueue);
-                   
-                   
+                    Operations.MoveBasketToKassQueue(basketsQueue, kassQueue);
                 }
-                if (operationNumber == 3)
+                else if (operationNumber == 3)
                 {
-
+                    Console.WriteLine("Twój wybór: 3");                    
+                    
+                    Operations.DecisionYorN(basketsQueue, kassQueue);
+                    Operations.DisplayBasketsInKassQueue(kassQueue);
+                    break;
                 }
-                //else
-                //{
-                //    Console.WriteLine("Wprowadzono niepoprawną wartość. Wprowadź poprawną wartość: \n");
-                //}
+                else
+                {
+                    Console.WriteLine("Wprowadzono niepoprawną wartość. Wprowadź poprawną wartość: \n");
+                }
             }
         }
-
-
-
-
-
-
-
-
-
-
-        //List<string> currentList = queue.Dequeue();
-        //foreach (string product in currentList)
-        //{
-        //    Console.WriteLine(product);
-        //}
-
-
-
     }
-
-
 }
