2fd19a6787806e6eebce7977b08deb63e889d25a
Krzysztof Czekaj <krzysztofczekaj@gmail.com>
2024-03-17 19:18:30 +0100

    uzupełnienie o projekt 1_BMI

---
+++ 1_BMI/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+using System.Net.Http;
+
+namespace _1_BMI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Program wyliczający BMI dla osób w wieku od 19 lat.");
+            Console.WriteLine("Podaj imię:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Podaj wiek [liczba całkowita >= 19]:");
+
+        A:
+            string stringAge = Console.ReadLine();
+            int intAge;
+            if (!int.TryParse(stringAge, out intAge) || intAge < 19)
+            {
+                Console.WriteLine("Podano niewłaściwe dane. Podaj wiek [liczba całkowita >= 19]:");
+                goto A;
+            }
+
+            Console.WriteLine("Podaj wagę w [g]:");
+        B:
+            string stringWeight = Console.ReadLine();
+            double doubleWeight;
+            if (!double.TryParse(stringWeight, out doubleWeight))
+            {
+                Console.WriteLine("Podano niewłaściwe dane. Podaj wagę w [g]:");
+                goto B;
+            }
+
+        C:
+            Console.WriteLine("Podaj wzrost z dokładnością 2 miejsc po przecinku w [m]");
+            string stringHeight = Console.ReadLine();
+            double doubleHeight;
+            if (!double.TryParse(stringHeight, out doubleHeight))
+            {
+                Console.WriteLine("Podano niewłaściwe dane. Podaj wzrost z dokładnością 2 miejsc po przecinku w [m]:");
+                goto C;
+            }
+
+            double BMI = (doubleWeight / 1000) / (doubleHeight * doubleHeight);
+
+            bool gender;
+            char lastLetterOfName = (name[name.Length - 1]);
+            if (lastLetterOfName == 'A' || lastLetterOfName == 'a')
+            {
+                gender = true;
+            }
+            else { gender = false; }
+
+
+
+
+            WeightCalculator weightCalculator = new WeightCalculator();
+            bool isNormalWeight = weightCalculator.IsNormalWeight(intAge, BMI);
+
+            Console.WriteLine("Wpisz ścieżkę pliku do zapisu danych: ");
+
+          
+         D:   
+                string path = Console.ReadLine();
+            try
+            {
+                StreamWriter sw;
+                using (sw = new StreamWriter(path))
+                {
+                    sw.WriteLine("Imię: {0}", name);
+
+                    string genderText = (gender == true) ? ("K") : ("M");       //IO Exception
+                    sw.WriteLine("Płeć: {0}", genderText);
+                    sw.WriteLine("Wiek: {0}", intAge);
+                    sw.WriteLine("Waga [kg]: {0}", doubleWeight / 1000);
+                    sw.WriteLine("Wzrost [cm]: {0}", doubleHeight * 100);
+                    sw.WriteLine("BMI: {0}", Math.Round(BMI, 4));
+
+                    string isNormalWeightText = isNormalWeight ? ("TAK") : ("NIE");
+                    sw.WriteLine("Pożądana masa ciała: {0}", isNormalWeightText);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Wpisz poprawną ścieżkę. {0}.", e.Message);
+                goto D;
+
+            }
+        }               
+    }
+}
---
+++ 1_BMI/WeightCalculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_BMI
+{
+    public class WeightCalculator //w osobnym pliku - zrobione
+    {
+
+        private bool CheckBMI(double minBMI, double maxBMI, double BMI)
+        {
+
+            if (BMI >= minBMI && BMI <= maxBMI)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+
+        public bool IsNormalWeight(int age, double BMI)
+        {
+            int liczba = 0;
+            switch (liczba)
+            {
+                case 1 when (age >= 19 && age <= 24):
+                    return CheckBMI(19, 24, BMI);
+                    break;
+                case 2 when (age <= 34 && BMI >= 20 && BMI <= 25):
+                    return CheckBMI(20, 25, BMI);
+                    break;
+                case 3 when (age <= 44 && BMI >= 21 && BMI <= 26):
+                        return CheckBMI(21, 26, BMI);
+                    break;
+                case 4 when (age <= 54 && BMI >= 22 && BMI <= 27):
+                    return CheckBMI(22, 27, BMI);
+                    break;
+                case 5 when (age <= 64 && BMI >= 23 && BMI <= 28):
+                    return CheckBMI(23, 28, BMI);
+                    break;
+                case 6 when (age > 64 && BMI >= 24 && BMI <= 29):
+                    return CheckBMI(24, 29, BMI);
+                    break;
+
+                default:
+                    return false;
+                    Console.WriteLine("niewłaściwy wiek");
+                    break;
+            }
+        }
+    }
+}
