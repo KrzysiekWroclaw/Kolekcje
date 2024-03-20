c1f21b42daf7d98e753f9eda0066ec5edd334007
Krzysztof Czekaj <krzysztofczekaj@gmail.com>
2024-03-20 02:16:15 +0100

    SprawdzaniePoprawnosciNawiasow - wersja 1

---
+++ SprawdzaniePoprawnosciNawiasow/CheckBrackets.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprawdzaniePoprawnosciNawiasow
+{
+    public class CheckBrackets
+    {
+        public static void iterateThroughCharacters(string entireText)
+        {
+            Stack<char> openBracketsStack = new Stack<char>();
+            
+            foreach (char character in entireText)
+            {
+                if (character == '(' || character == '[' || character == '{')
+                {
+                    openBracketsStack.Push(character);
+                }
+
+                char lastOpenedBracket;
+                if ((character == ')' || character == ']' || character == '}'))
+                {
+                    lastOpenedBracket = openBracketsStack.Pop();
+
+                    if (character == ')' && lastOpenedBracket != '(')
+                    {
+                        Console.WriteLine("Napotkano nawias zamykający ) bez wcześniejszego nawiasu otwierającego.");
+                    }
+                    if (character != ')' && lastOpenedBracket == '(')
+                    {
+                        Console.WriteLine("Brakuje nawiasu zamykającego ).");
+                    }
+                    if (character == ')' && (lastOpenedBracket == '['))
+                    {
+                        Console.WriteLine("Nawias zamykający ) nie pasuje do otwierającego [. ");
+                    }
+                    if (character == ')' && lastOpenedBracket == '{')
+                    {
+                        Console.WriteLine("Nawias zamykający ) nie pasuje do otwierającego {. ");
+                    }
+
+
+
+                    if (character == ']' && lastOpenedBracket != '[')
+                    {
+                        Console.WriteLine("Napotkano nawias zamykający ] bez wcześniejszego nawiasu otwierającego.");
+                    }
+                    if (character != ']' && lastOpenedBracket == '[')
+                    {
+                        Console.WriteLine("Brakuje nawiasu zamykającego ].");
+                    }
+                    if (character == ']' && (lastOpenedBracket == '('))
+                    {
+                        Console.WriteLine("Nawias zamykający ] nie pasuje do otwierającego (. ");
+                    }
+                    if (character == ']' && lastOpenedBracket == '{')
+                    {
+                        Console.WriteLine("Nawias zamykający ) nie pasuje do otwierającego {. ");
+                    }
+
+
+
+
+                    if (character == '}' && lastOpenedBracket != '{')
+                    {
+                        Console.WriteLine("Napotkano nawias zamykający } bez wcześniejszego nawiasu otwierającego.");
+                    }
+                    if (character != '}' && lastOpenedBracket == '{')
+                    {
+                        Console.WriteLine("Brakuje nawiasu zamykającego }.");
+                    }
+                    if (character == '}' && (lastOpenedBracket == '('))
+                    {
+                        Console.WriteLine("Nawias zamykający } nie pasuje do otwierającego (. ");
+                    }
+                    if (character == '}' && lastOpenedBracket == '[')
+                    {
+                        Console.WriteLine("Nawias zamykający } nie pasuje do otwierającego [. ");
+                    }
+                }
+                    
+                
+            }
+        }
+    }
+}
--- SprawdzaniePoprawnosciNawiasow/Program.cs
+++ SprawdzaniePoprawnosciNawiasow/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+
+namespace SprawdzaniePoprawnosciNawiasow
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Program: Sprawdzanie poprawności nawiasów.\n" +
+                             "Wprowadź wyrażenie matematyczne: ");
+
+            string? entireText = Console.ReadLine();
+
+            CheckBrackets.iterateThroughCharacters(entireText);
+            
+            
+        }
+    }
+}
