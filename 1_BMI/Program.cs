6d6465512f45f3e5a6de95e4da750904398a810d
Krzysztof Czekaj <krzysztofczekaj@gmail.com>
2024-03-25 19:19:53 +0100

    Podmianka niewłaściwych plików w "_1_BMI".

--- 1_BMI/Program.cs
+++ 1_BMI/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Xml.Linq;
 using System.Net.Http;
+using static System.Net.Mime.MediaTypeNames;
+using System.ComponentModel.Design;
 
 namespace _1_BMI
 {
@@ -14,38 +16,46 @@ namespace _1_BMI
         public static void Main(string[] args)
         {
             Console.WriteLine("Program wyliczający BMI dla osób w wieku od 19 lat.");
+
             Console.WriteLine("Podaj imię:");
-            string name = Console.ReadLine();
+            string? name = Console.ReadLine();
 
             Console.WriteLine("Podaj wiek [liczba całkowita >= 19]:");
-
-        A:
-            string stringAge = Console.ReadLine();
             int intAge;
-            if (!int.TryParse(stringAge, out intAge) || intAge < 19)
+            while (true)
             {
-                Console.WriteLine("Podano niewłaściwe dane. Podaj wiek [liczba całkowita >= 19]:");
-                goto A;
+                string stringAge = Console.ReadLine();
+                if (!int.TryParse(stringAge, out intAge) || intAge < 19)
+                {
+                    Console.WriteLine("Podano niewłaściwe dane. Podaj wiek [liczba całkowita >= 19]:");
+                }
+                else { break; }
             }
 
             Console.WriteLine("Podaj wagę w [g]:");
-        B:
-            string stringWeight = Console.ReadLine();
             double doubleWeight;
-            if (!double.TryParse(stringWeight, out doubleWeight))
+            while (true)
             {
-                Console.WriteLine("Podano niewłaściwe dane. Podaj wagę w [g]:");
-                goto B;
+                string stringWeight = Console.ReadLine();
+                if (!double.TryParse(stringWeight, out doubleWeight))
+                {
+                    Console.WriteLine("Podano niewłaściwe dane. Podaj wagę w [g]:");
+                }
+                else { break; }
             }
 
-        C:
+
+
             Console.WriteLine("Podaj wzrost z dokładnością 2 miejsc po przecinku w [m]");
-            string stringHeight = Console.ReadLine();
             double doubleHeight;
-            if (!double.TryParse(stringHeight, out doubleHeight))
+            while (true)
             {
-                Console.WriteLine("Podano niewłaściwe dane. Podaj wzrost z dokładnością 2 miejsc po przecinku w [m]:");
-                goto C;
+                string stringHeight = Console.ReadLine();
+                if (!double.TryParse(stringHeight, out doubleHeight))
+                {
+                    Console.WriteLine("Podano niewłaściwe dane. Podaj wzrost z dokładnością 2 miejsc po przecinku w [m]:");
+                }
+                else { break; }
             }
 
             double BMI = (doubleWeight / 1000) / (doubleHeight * doubleHeight);
@@ -58,41 +68,41 @@ namespace _1_BMI
             }
             else { gender = false; }
 
-
-
-
             WeightCalculator weightCalculator = new WeightCalculator();
             bool isNormalWeight = weightCalculator.IsNormalWeight(intAge, BMI);
 
-            Console.WriteLine("Wpisz ścieżkę pliku do zapisu danych: ");
 
-          
-         D:   
-                string path = Console.ReadLine();
-            try
+            while (true)
             {
-                StreamWriter sw;
-                using (sw = new StreamWriter(path))
+                try
                 {
+                    Console.WriteLine("Wpisz ścieżkę pliku do zapisu danych: ");
+                    string path = Console.ReadLine();
+                    using StreamWriter sw = new StreamWriter(path);
                     sw.WriteLine("Imię: {0}", name);
-
-                    string genderText = (gender == true) ? ("K") : ("M");       //IO Exception
+                    string genderText = (gender) ? ("K") : ("M");
                     sw.WriteLine("Płeć: {0}", genderText);
                     sw.WriteLine("Wiek: {0}", intAge);
                     sw.WriteLine("Waga [kg]: {0}", doubleWeight / 1000);
                     sw.WriteLine("Wzrost [cm]: {0}", doubleHeight * 100);
                     sw.WriteLine("BMI: {0}", Math.Round(BMI, 4));
-
                     string isNormalWeightText = isNormalWeight ? ("TAK") : ("NIE");
                     sw.WriteLine("Pożądana masa ciała: {0}", isNormalWeightText);
+                    break;
+
+                }
+                catch (System.ArgumentException e)
+                {
+
+                }
+                catch (IOException e2)
+                {
+                    Console.WriteLine("Niepoprawna ścieżka pliku.");
                 }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("Wpisz poprawną ścieżkę. {0}.", e.Message);
-                goto D;
-
-            }
-        }               
+        }
     }
 }
+
+
+
